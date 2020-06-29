5a41bd44a8d7892d1cb7aee60aba5f0cff684075
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-06-29 07:29:51 +0200

    adds constructor/tsdoc for "All"

--- lib/shared/all.ts
+++ lib/shared/all.ts
@@ -1,9 +1,18 @@
+import { PolicyStatementProps } from '@aws-cdk/aws-iam';
+
 import { PolicyStatement } from './policy-statement';
 
 /**
  * Action provider for all services
  */
 export class All extends PolicyStatement {
+  /**
+   * Action provider for all services
+   */
+  constructor(props?: PolicyStatementProps) {
+    super(props);
+  }
+
   /**
    * Adds all actions to the statement: `actions: '*'`
    */
