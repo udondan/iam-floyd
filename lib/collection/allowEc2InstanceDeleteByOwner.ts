d345e7c094dec21e4ef8d3e99b7425dcfd26c3f0
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-07-27 10:24:05 +0200

    implements new .ifAwsRequestTag condition

--- lib/collection/allowEc2InstanceDeleteByOwner.ts
+++ lib/collection/allowEc2InstanceDeleteByOwner.ts
@@ -3,17 +3,16 @@ import * as statement from '..';
 /**
  * Allows stopping EC2 instance only for the user who started them
  *
- * TODO: describe tag param, default value
+ * @param tag The tag name, where the user information will be stored - default: `Owner`
  */
 
 export function allowEc2InstanceDeleteByOwner(tag?: string) {
   const tagName = tag || 'Owner';
-
   return [
-    new statement.Ec2().allow().startInstances().if('StringEquals', {
-      // TODO: use the tag instead of hardcoded 'Owner'
-      'aws:RequestTag/Owner': '${aws:username}',
-    }),
+    new statement.Ec2()
+      .allow()
+      .startInstances()
+      .ifAwsRequestTag(tagName, '${aws:username}'),
     new statement.Ec2()
       .allow()
       .stopInstances()
