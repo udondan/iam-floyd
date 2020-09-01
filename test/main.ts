3dc698773b594efeeddb19c1ef37a28fc73c8088
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-09-01 20:59:40 +0200

    adds main test file

---
+++ test/main.ts
@@ -0,0 +1,25 @@
+import * as statement from '../lib';
+
+function printPolicyWitStatements(statements: any[]) {
+  console.log(
+    JSON.stringify(
+      {
+        Version: '2012-10-17',
+        Statement: [...statements],
+      },
+      null,
+      2
+    )
+  );
+}
+
+printPolicyWitStatements(
+  new statement.Collection().allowEc2InstanceDeleteByOwner()
+);
+
+printPolicyWitStatements(
+  new statement.Secretsmanager()
+    .allow()
+    .allActions()
+    .ifResourceTag('CreatedBy', 'me')
+);
