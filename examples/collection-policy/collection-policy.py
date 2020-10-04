dc25c04e3d09996bae4d8bf7f8dbcf3dee7b093f
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-10-04 14:30:09 +0200

    align quotes with other examples

--- examples/collection-policy/collection-policy.py
+++ examples/collection-policy/collection-policy.py
@@ -4,6 +4,6 @@ import iam_floyd as statement
 statements = statement.Collection().name_of_the_collection()
 
 policy = {
-    "Version": "2012-10-17",
-    "Statement": list(map(lambda x: x.to_json(), statements)),
+    'Version': '2012-10-17',
+    'Statement': list(map(lambda x: x.to_json(), statements)),
 }
