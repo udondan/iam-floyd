23f335405f8ad59f1b1637da4c7dac344f931eda
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-10-04 11:24:24 +0200

    example styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,6 +42,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+html_css_files = [
+    'css/custom.css',
+]
+
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -54,3 +58,5 @@ exclude_patterns = []
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+pygments_style = 'monokai'
