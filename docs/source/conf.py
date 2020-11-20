af60b367f914e79af13db10ee4373f5183ad1ed7
GitHub Action <action@github.com>
2020-11-20 06:49:29 +0000

    Finalize v0.95.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Daniel Schroeder'
 author = 'Daniel Schroeder'
 
 # The full version, including alpha/beta/rc tags
-release = '0.95.0'
+release = '0.95.1'
 
 # -- General configuration ---------------------------------------------------
 
