6ba7227976b8259ed86adb8b581fdd10067719ee
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2020-12-20 11:16:51 +0100

    switch local theme to RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,9 @@ release = '0.108.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 sys.path.append(os.path.abspath('extensions'))
-extensions = ['sphinx_tabs.tabs', 'example']
+extensions = ['sphinx_rtd_theme', 'sphinx_tabs.tabs', 'example']
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
