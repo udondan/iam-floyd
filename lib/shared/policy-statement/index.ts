b6120877fb7b5a5e050ddf876eb832b9de6c2f49
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2021-04-05 09:31:21 +0200

    still fucked

--- lib/shared/policy-statement/index.ts
+++ lib/shared/policy-statement/index.ts
@@ -1,8 +1,2 @@
-//export { ResourceTypes } from './4-resources';
 export { PolicyStatement } from './9-final';
-//export { PolicyStatementWithPrincipal, Principals } from './6-principals';
-//export { PolicyStatementWithEffect, Effect } from './5-effect';
-//export { PolicyStatementWithResources } from './4-resources';
-//export { PolicyStatementWithActions } from './3-actions';
-//export { PolicyStatementWithCondition, Conditions } from './2-conditions';
 export { PolicyStatementBase } from './1-base';
