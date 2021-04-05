397d3f25d1d565c2fb65c9cbee82c6e6bb0d33e0
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2021-04-05 09:36:29 +0200

    still fucked

--- lib/shared/policy-statement/9-final.ts
+++ lib/shared/policy-statement/9-final.ts
@@ -1,8 +1,6 @@
-import { PolicyStatementBase } from './1-base';
-
 /**
  * Represents a statement in an IAM policy document
  */
-export class PolicyStatement extends PolicyStatementBase {
+export class PolicyStatement {
   protected cdkApplied = false; // internally used to check if resources, actions and conditions have already been applied to the policy
 }
