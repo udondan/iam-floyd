d388b3e304bdf567388403a043048ce37a5b0350
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2021-07-06 17:45:14 +0200

    reset shared package

--- lib/shared/all.ts
+++ lib/shared/all.ts
@@ -9,8 +9,9 @@ export class All extends PolicyStatement {
    *
    * Use this provider to globally allow/deny actions, based on global conditions.
    *
+   * @param sid [SID](https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_sid.html) of the statement
    */
-  constructor(props?: iam.PolicyStatementProps) {
-    super(props);
+  constructor(sid?: string) {
+    super(sid);
   }
 }
