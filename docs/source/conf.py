b73c5064b270ee18a1d89fb1ffec1873ad2a24db
udondan <deemes79@googlemail.com>
2021-08-07 02:23:39 +0000

    Adds latest IAM updates from AWS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Daniel Schroeder'
 author = 'Daniel Schroeder'
 
 # The full version, including alpha/beta/rc tags
-release = '0.223.0'
+release = '0.224.0'
 
 # -- General configuration ---------------------------------------------------
 
