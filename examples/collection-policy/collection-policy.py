2d59fea15cee668519beae44b73b725c5458b3fa
daniel.schroeder <daniel.dn.schroeder@deutschebahn.com>
2022-04-13 18:30:59 +0200

    more tests

--- examples/collection-policy/collection-policy.py
+++ examples/collection-policy/collection-policy.py
@@ -1,5 +1,9 @@
 import json
 import iam_floyd as statement
+import importlib
+import os
+import sys
+import inspect
 
 currentdir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
