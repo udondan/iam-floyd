dcdab2aef2f7d7fa03ad4834ebdfa4825a5cc975
GitHub Action <action@github.com>
2024-02-17 16:34:06 +0000

    Finalize v0.603.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2020, Daniel Schroeder'
 author = 'Daniel Schroeder'
 
 # The full version, including alpha/beta/rc tags
-release = '0.602.0'
+release = '0.603.0'
 
 # -- General configuration ---------------------------------------------------
 
