41790f1e79e6aef7797d12a0c81c4130a0a233a7
udondan <deemes79@googlemail.com>
2026-01-18 02:42:20 +0000

    Adds latest IAM updates from AWS

--- README.md
+++ README.md
@@ -17,9 +17,9 @@
 Support for:
 
 - 441 Services
-- 20079 Actions
-- 2137 Resource Types
-- 2235 Condition keys
+- 20028 Actions
+- 2130 Resource Types
+- 2230 Condition keys
 <!-- /stats -->
 
 ![EXPERIMENTAL](https://img.shields.io/badge/stability-experimantal-orange?style=for-the-badge)**<br>This is an early version of the package. The API will change while I implement new features. Therefore make sure you use an exact version in your `package.json` before it reaches 1.0.0.**
--- lib/generated/policy-statements/novaact.ts
+++ lib/generated/policy-statements/novaact.ts
@@ -2,7 +2,7 @@ import { AccessLevelList } from '../../shared/access-level';
 import { PolicyStatement } from '../../shared';
 
 /**
- * Statement provider for service [nova-act](https://docs.aws.amazon.com/service-authorization/latest/reference/list_awsservicetomanageaiagentsforuiworkflows.html).
+ * Statement provider for service [nova-act](https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonnovaact.html).
  *
  * @param sid [SID](https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_sid.html) of the statement
  */
@@ -10,7 +10,7 @@ export class NovaAct extends PolicyStatement {
   public servicePrefix = 'nova-act';
 
   /**
-   * Statement provider for service [nova-act](https://docs.aws.amazon.com/service-authorization/latest/reference/list_awsservicetomanageaiagentsforuiworkflows.html).
+   * Statement provider for service [nova-act](https://docs.aws.amazon.com/service-authorization/latest/reference/list_amazonnovaact.html).
    *
    * @param sid [SID](https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_sid.html) of the statement
    */
